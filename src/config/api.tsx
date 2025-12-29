3235d7bff2e82336847e10de4f51567a19f66599
Chien <chien1472k4@gmail.com>
2025-12-29 22:00:16 +0700

    test ci/cd

--- src/config/api.tsx
+++ src/config/api.tsx
@@ -1,3 +1,3 @@
-export const BASE_API_URL = "http://192.168.1.60:8080/";
-export const WEBSOCKET_URL = "http://192.168.1.60:8080/ws";
+export const BASE_API_URL = "http://localhost:8080/";
+export const WEBSOCKET_URL = "http://localhost:8080/ws";
 export const FE_URL = "http://localhost:5173";
