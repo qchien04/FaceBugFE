3dd575247b530b935c6f11a83965fd04e298cbeb
Chien <chien1472k4@gmail.com>
2026-01-06 17:21:25 +0700

    update gg login

--- src/config/api.tsx
+++ src/config/api.tsx
@@ -1,3 +1,3 @@
 export const BASE_API_URL = "http://localhost:8080/";
 export const WEBSOCKET_URL = "http://localhost:8080/ws";
-export const FE_URL = "http://anbato.site";
+export const FE_URL = "https://anbato.site";
