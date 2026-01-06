9427cd6017ff91c50cc705b9e2669f94ba4e451b
Chien <chien1472k4@gmail.com>
2026-01-06 22:20:07 +0700

    ok

--- src/config/api.tsx
+++ src/config/api.tsx
@@ -1,3 +1,3 @@
-export const BASE_API_URL = "http://localhost:8080/";
-export const WEBSOCKET_URL = "http://localhost:8080/ws";
+export const BASE_API_URL = "https://anbato.site/api/";
+export const WEBSOCKET_URL = "https://anbato.site/api/ws";
 export const FE_URL = "https://anbato.site";
